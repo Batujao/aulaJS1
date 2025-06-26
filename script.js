f086c0d0ff97dc1873d562e86fe05363581d7b0c
Batujao <joaoantoniel28@gmail.com>
2025-06-25 21:31:28 -0300

    update final

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿alert("Seja bem-vindo ao nosso jogo!");
+
+// Variável para armazenar o chute do usuário
+let qtd;
+qtd = prompt("Digite um numero pra fazer a media: ");
+
+let i;
+let soma = 0;
+for (i = 1; i <= qtd; i++) {
+  soma = soma + i;
+}
+let media = soma / qtd;
+alert(`${soma}`);
+alert(`Sua média é: ${media}`);
